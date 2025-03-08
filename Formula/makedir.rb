e5e2c98fb4697d56c233ba8848d2c2b55e1b71de
soup-ms <michaelsousajr@gmail.com>
2025-03-08 16:57:25 -0500

    Update makedir to v0.2.2

--- Formula/makedir.rb
+++ Formula/makedir.rb
@@ -1,8 +1,8 @@
 class Makedir < Formula
   desc "A better mkdir command: Create directories with predefined setups"
   homepage "https://github.com/soup-ms/makedir"
-  url "https://github.com/soup-ms/makedir/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "4d1ec37bd6e205bf280c826a71d7ee409c41e8a53ed41b28bb3de72ce2aa847f"
+  url "https://github.com/soup-ms/makedir/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "74e317d7dc190e80f021242d0a1138600b09580e3a6e89bc39671ce65cf67211"
   license "MIT"
 
   depends_on "rust" => :build
